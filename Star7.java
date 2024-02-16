38e1f2551655f7dfc29be9da33e0cf89f630a025
Santosh-Rashinkar <santoshrashinkar0108@gmail.com>
2024-02-16 19:08:07 +0530

    Star patterns

---
+++ Star7.java
@@ -0,0 +1,23 @@
+public class Star8{
+
+public static void main(String[] args){
+
+for(int i=1;i<=5;i++){
+
+    for(int j=5;j>=i;j--){
+     System.out.print(" ");
+    }
+    for(int k=1;k<=i;k++){
+    System.out.print(" *");
+    }
+    for(int l=2;l<=i;l++){
+System.out.print("*");
+
+}
+System.out.println();
+}
+
+}
+}
+}
+
